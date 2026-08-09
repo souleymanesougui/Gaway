b8174c4b997ec592c0e41c80fe05bed7b881bee2
souleymanesougui <souguisouleymane7@gmail.com>
2026-08-09 11:25:35 +0100

    nouvelle modification

--- firebase.js
+++ firebase.js
@@ -9,7 +9,7 @@ import {
 } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AlzaSyDh1sYrwm5dTG3ktVHprKzEGr7fRmzOzfQ",  // 👈 "Alza" et non "AIza"
+  apiKey: "AIzaSyDh1sYrwm5dTG3ktVHprKzEGr7fRmxOzfQ",  // 👈 "Alza" et non "AIza"
   authDomain: "gaway-290bc.firebaseapp.com",
   projectId: "gaway-290bc",
   storageBucket: "gaway-290bc.firebasestorage.app",
